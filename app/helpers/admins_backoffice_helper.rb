84759c33e43e3a09683ffa9ac162faaf47eb76e3
André Araujo <andreguia@hotmail.com>
2019-08-21 00:56:29 +0000

    Mod_5 - Aulas 168-174

--- app/helpers/admins_backoffice_helper.rb
+++ app/helpers/admins_backoffice_helper.rb
@@ -1,2 +1,10 @@
 module AdminsBackofficeHelper
+
+  def translate_method(object = nil, method = nil)
+    if object && method
+      object.model.human_attribute_name(method)
+    else
+      "Message"
+    end
+  end
 end
